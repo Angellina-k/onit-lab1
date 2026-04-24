6484eb1d4eca73e71bd626a2d67a4d40e832c373
Angellina-k <kadamovaam@mail.ru>
2026-04-24 12:36:09 +0300

    add functional test

---
+++ tests/test_app.py
@@ -0,0 +1,18 @@
+import os
+import sys
+from unittest.mock import patch
+
+sys.path.insert(0, os.path.abspath("app"))
+
+from app import app
+
+
+def test_home_page():
+    with patch("routes.Task") as mock_task:
+        mock_task.query.all.return_value = []
+
+        client = app.test_client()
+        response = client.get("/")
+
+        assert response.status_code == 200
+        assert "Список задач".encode("utf-8") in response.data
